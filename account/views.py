1f849c84276b0be1c0b2b924846f10f780b5602c
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-09 21:44:12 +0100

    added add user view

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get("username")
@@ -12,6 +13,29 @@ def login_view(request):
     else:
         return render(request, "account/login.html")
 
+
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+
+def add_user(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+        else
+        pass
+    else:
+        pass
+    context = {}
+    return render(request, 'add_user.html', context=context)
+
+
+def change_password(request):
+    pass
+
+
+def recover_password(request):
+    pass
