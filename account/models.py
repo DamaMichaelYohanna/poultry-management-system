e962f86ae65b99f4f82673649a6cbc84da6a9d7d
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-09 21:36:13 +0100

    create profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+user = get_user_model()
+
+
+class Profile(models.Model):
+    """model for user's profile """
+    user = models.ForeignKey(user, on_delete=models.CASCADE, )
+    contact = models.CharField(max_length=12)
+    address = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_created=True)
+    image = models.ImageField()
