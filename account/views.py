00bb7c4c8c2a86520d9ac73352234f9aaaaf72ac
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-09 12:15:43 +0100

    add login and logout

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username, password)
+        if user:
+            login(request, user)
+        return redirect('/')
+    else:
+        return render(request, "account/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
