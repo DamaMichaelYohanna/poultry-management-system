b2278616976110e81bb6a935b916944c42bb814c
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-10 06:41:23 +0100

    update urls

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 from account import views
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
 ]
