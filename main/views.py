4a08ae0458cf8169fbe4e49a192b691dd294739f
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-21 21:18:34 +0100

    added sample code

--- main/views.py
+++ main/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def store(request):
+    pass
+
+
+def farm_detail(request):
+    pass
+
+
+def sell_product(request):
+    pass
+
+
+def invoice(request):
+    pass
