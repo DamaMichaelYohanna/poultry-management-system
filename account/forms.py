a185c766e8b18227243a3e633b5ae667da74ff2e
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-23 13:39:51 +0100

    update UI for update_profile.html

--- account/forms.py
+++ account/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-
 from account.models import Profile
+
+
 class UserForm(forms.ModelForm):
     """good with arrows"""
 
     class Meta:
-        get_user_model()
-        fields = "__all__"
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
@@ -16,4 +17,4 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = "__all__"
+        exclude = ("user",)
