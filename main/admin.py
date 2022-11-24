229676fc2b10b9886bdbc2ef4f4ea6f09ad62d66
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-24 12:48:07 +0100

    added model to admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, Store, Farm
+
+admin.site.register(Item)
+admin.site.register(Store)
+admin.site.register(Farm)
