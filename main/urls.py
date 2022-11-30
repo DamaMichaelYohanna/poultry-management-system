dce819e65129eb5fe21ad2c296b4750027705bbf
Dama Michael Yohanna <get2dama11@gmail.com>
2022-11-30 14:36:14 +0100

    update urls

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('farm', views.farm_detail, name='farm'),
     path('store', views.Store.as_view(), name='store'),
     path('store/add/item', views.add_to_store_item, name='add item'),
-    path("store/pickout", views.pick_out, name='pickout'),
+    path("store/pickout/<int:pk>", views.pick_out, name='pickout'),
 
 ]
