56d40c0b54c72d6f629290f9808ccf60c52f2993
Dama Michael Yohanna <get2dama11@gmail.com>
2022-12-09 12:12:43 +0100

    update admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Item, Store, Farm
+from .models import Item, Store, Farm, Product, Category
 
 admin.site.register(Item)
 admin.site.register(Store)
 admin.site.register(Farm)
+admin.site.register(Product)
+admin.site.register(Category)
