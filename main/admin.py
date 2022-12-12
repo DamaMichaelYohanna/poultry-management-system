1b073c59674e593596a27cca1d376382c689fa3d
Dama Michael Yohanna <get2dama11@gmail.com>
2022-12-12 07:52:55 +0100

    update admin file

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Item, Store, Farm, Product, Category
+from .models import Item, Store, Farm, Product, Category, Order
 
 admin.site.register(Item)
 admin.site.register(Store)
 admin.site.register(Farm)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Order)
