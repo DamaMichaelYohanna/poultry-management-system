f3d0358757fc9ddc35b9bcb79ccf91086630968c
Dama Michael Yohanna <get2dama11@gmail.com>
2022-12-12 07:54:44 +0100

    update urls file

--- main/urls.py
+++ main/urls.py
@@ -12,10 +12,14 @@ urlpatterns = [
     path('store/item/restock', views.restock, name='restock'),
     path("store/item/pickout/<int:pk>", views.pick_out, name='pickout'),
 #     urls for products
-    path('product/dashboard', views.product_dashboard, name='product dashboard'),
+    path('product/dashboard', views.product_dashboard,
+         name='product_dashboard'),
     path('product/add', views.product_add, name='product add'),
     path('product/update', views.product_update, name='product update'),
-    path('product/delete/<int:pk>', views.product_delete, name='product delete'),
-    path('product/add_to_cart', views.add_to_cart, name='add to cart')
+    path('product/delete/<int:pk>', views.product_delete,
+         name='product delete'),
+    path('product/add_to_cart/<int:pk>', views.add_to_cart, name='add to cart'),
+    path('product/remove_from_cart/<int:pk>', views.remove_from_cart,
+         name='remove from cart'),
 
 ]
