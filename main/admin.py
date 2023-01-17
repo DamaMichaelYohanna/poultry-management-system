939e1888b66478ad74728305ec23ffc3b6568f89
Dama Michael Yohanna <get2dama11@gmail.com>
2023-01-17 03:51:25 +0100

    updated admin site: added new models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Item, Store, Farm, Product, Category, Order
+from .models import Item, Store, Farm, Product, Category, Order, Invoice, InvoiceProduct
 
 admin.site.register(Item)
 admin.site.register(Store)
@@ -8,3 +8,5 @@ admin.site.register(Farm)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Order)
+admin.site.register(Invoice)
+admin.site.register(InvoiceProduct)
